58f851c5bffc09350912bddc0601f53e73f978e9
Theequilizer <rohanmathur10b@gmail.com>
2020-02-01 00:14:51 +0530

    PWM

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -1,13 +1,19 @@
 #include<Wire.h>
 #define SLAVE_ADDRESS 0x8
+int in1 = 4;
+int in2 = 5;
 int in3 = 6; 
-int in4 = 7; 
+int in4 = 7;
+int enA = 9; 
 int enB = 10; 
 byte arr[]={0,0,0,0};
 void setup() {
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enA, OUTPUT);
   pinMode(in3, OUTPUT); //Declaring the pin modes, obviously they're outputs
   pinMode(in4, OUTPUT);
   pinMode(enB, OUTPUT);
@@ -24,28 +30,44 @@ void receiveEvent(int howMany) {
   Serial.println(arr[2]);
   Serial.println(arr[3]);
 }
-
-if (arr[0]==1){
-  //void TurnMotorA(){
-  Serial.println("front");
+if(arr[0]==0){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enB,255);
+  analogWrite(enA,255);
+}
+else if (arr[0]==1){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  analogWrite(enB,arr[3]);
-//}  
-}
-else if(arr[0]==0){
-  //void TurnOFFA(){
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  analogWrite(enB,100);
-//}
-}
+  analogWrite(enB,255);
+  analogWrite(enA,255); 
+  }
 else if(arr[0]==2){
-//void TurnMotorA2(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW); 
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-  analogWrite(enB,arr[3]);
-//}
+  analogWrite(enB,255);
+  analogWrite(enA,255);
+}
+else if(arr[0]==3){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enB,255);
+  analogWrite(enA,255);
+}
+else if(arr[0]==4){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB,100);
 }
 }
 void loop() {
